772240f4bc6f3ffc56a1f923f88ce31a48691a1e
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-11-28 22:11:06 +0900

    :tada:`x.x.x`(root): Create raycast-extensions

---
+++ .githooks/commitHook.ts
@@ -0,0 +1,77 @@
+import * as hoipoiCapsule from "https://deno.land/x/hoipoi_capsule/mod.ts";
+import { $ as zx } from "npm:zx";
+
+const o =
+  await hoipoiCapsule.preset.fillInCommitMessage.gitmojiStyle.initialize();
+
+const packages = (await zx`ls packages/`.quiet()).stdout
+  .split("\n")
+  .filter(Boolean)
+  .map((v) => ({ name: v, value: v }));
+
+const line = {
+  name: hoipoiCapsule.userInterface.prompt.colors.yellow("---------"),
+  value: "",
+  disabled: true,
+};
+
+const gitmojiQ = () =>
+  hoipoiCapsule.userInterface.prompt.Select.prompt({
+    message: "Select gitmoji.",
+    search: true,
+    options: o.gitmojis,
+  });
+
+const scopeQ = () =>
+  hoipoiCapsule.userInterface.prompt.Select.prompt({
+    message: "Select scope.",
+    search: true,
+    options: [
+      { name: "_", value: "_" },
+      { name: "root", value: "root" },
+      line,
+    ].concat(packages),
+  });
+
+const issueQ = () =>
+  hoipoiCapsule.userInterface.prompt.Select.prompt({
+    message: "Select issue.",
+    search: true,
+    options: o.issues,
+  });
+
+const commitMessageTemplate = `{{gitmoji}}({{scope}}): {{summary}} Close #{{issue}}
+
+{{body}}`;
+
+hoipoiCapsule.useCase.fillInCommitMessage.run({
+  commitMessageTemplate,
+  questionList: [
+    {
+      target: "gitmoji",
+      q: gitmojiQ,
+    },
+    {
+      target: "scope",
+      q: scopeQ,
+    },
+    {
+      target: "summary",
+      q: hoipoiCapsule.preset.fillInCommitMessage.gitmojiStyle.qMap.summary,
+    },
+    {
+      target: "issue",
+      q: issueQ,
+      fixCommitMessage:
+        hoipoiCapsule.preset.fillInCommitMessage.gitmojiStyle
+          .fixCommitMessageMap.issue,
+    },
+    {
+      target: "body",
+      q: hoipoiCapsule.preset.fillInCommitMessage.gitmojiStyle.qMap.bodyQ,
+      fixCommitMessage:
+        hoipoiCapsule.preset.fillInCommitMessage.gitmojiStyle
+          .fixCommitMessageMap.body,
+    },
+  ],
+});
---
+++ README.md
@@ -0,0 +1,16 @@
+# Raycasts
+
+## Development
+
+### Raycast プラグインを追加
+
+ファイル更新
+
+- .github/dependabot.yml
+- .github/labeler.yml
+- .github/workflows/release-drafter.yml
+- package.json
+
+ファイル追加
+
+- .github/release-drafter-`new-plugin`.yml
